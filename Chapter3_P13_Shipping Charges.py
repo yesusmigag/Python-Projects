ef0a3e8952275157cf25a2193c67493e7854d7f0
yesusmigag <51185179+yesusmigag@users.noreply.github.com>
2021-03-02 23:28:35 -0500

    Chapter 3 Problem 13

---
+++ Chapter3_P13_Shipping Charges.py
@@ -0,0 +1,38 @@
+"""			      	
+	Chapter #2,Problem #6 Sales Tax	
+
+	This program will calculate the  Sales Tax
+	-------------------------------------------------------
+	INPUT							OUTPUT
+	-----							------
+	Amount of Purchase				Amount of Purchasee
+						            Country Sales Tax
+	                                State Sales Tax
+                                    Total Sales Tax
+	-------------------------------------------------------
+"""
+#****Module\Libraries
+import os
+#*** Program modules, functions, constants, and variables
+#*** Declaration and initialization of variables
+
+
+#*** BEGINNING OF THE PROGRAM LOGIC ****
+
+#*** Description of the program
+os.system("clear")          #*** Clear screen
+
+
+#*** Request data
+
+#*** Process data
+
+
+#*** Display results
+
+
+
+
+#***** Ending program
+print("\n")   
+#***** END executions of the program    
